b13c0956efc5ff8afa8d068ce5ea9736608169e3
Nicolaas Smit <nick.smit@rtk.io>
2020-07-28 20:31:59 +0200

    logging

--- processors/GetIntradayPrices/config-exports.js
+++ processors/GetIntradayPrices/config-exports.js
@@ -1,11 +1,12 @@
 const config = {
-    cognito_pool_id: "eu-west-1_DMhZ3rrXS",
-    cognito_backend_client_id: "pq3rcra4kend08leir0lbth06",
-    cognito_backend_access_key: "stockMonitoring/local/backend",
-    appsync_endpoint_url: "https://efrgcmtrnfcn7oshfxf2kvsx4y.appsync-api.eu-west-1.amazonaws.com/graphql",
-    appsync_region: "eu-west-1",
-    data_feed_key: "stockMonitoring/local/datafeed",
-    feed_interval: "30min"
-}
+  cognito_pool_id: "us-east-1_uv8S7qc1o",
+  cognito_backend_client_id: "39pdec5kiutd60jaftlirlgsg7",
+  cognito_backend_access_key: "stockMonitoring/me/backend",
+  appsync_endpoint_url:
+    "https://ur3ccj4lzbea7lvng3catocnxu.appsync-api.us-east-1.amazonaws.com/graphql",
+  appsync_region: "us-east-1",
+  data_feed_key: "stockMonitoring/me/datafeed",
+  feed_interval: "30min",
+};
 
-module.exports=config;
+module.exports = config;
--- processors/GetIntradayPrices/index.js
+++ processors/GetIntradayPrices/index.js
@@ -1,125 +1,145 @@
 const AWS = require("aws-sdk");
-const configExports = require('./config-exports');
-const AWSAppSyncClient = require('aws-appsync').default;
-const {listAllStockSymbols} = require('./graphql/queries');
-const {createIntradayStockPrice} = require('./graphql/mutations');
-const axios = require('axios');
+const configExports = require("./config-exports");
+const AWSAppSyncClient = require("aws-appsync").default;
+const { listAllStockSymbols } = require("./graphql/queries");
+const { createIntradayStockPrice } = require("./graphql/mutations");
+const axios = require("axios");
 const moment = require("moment-timezone");
 require("isomorphic-fetch");
 
-AWS.config.update({region: configExports.appsync_region});
+AWS.config.update({ region: configExports.appsync_region });
 
-const getBackendTokens = async(sm) => {
-    const idp = new AWS.CognitoIdentityServiceProvider();
-    const backendAdminCredentialsSecret = await sm.getSecretValue({SecretId: configExports.cognito_backend_access_key}).promise();
-    const backendAdminCredentials = JSON.parse(backendAdminCredentialsSecret.SecretString);
+const getBackendTokens = async (sm) => {
+  const idp = new AWS.CognitoIdentityServiceProvider();
+  const backendAdminCredentialsSecret = await sm
+    .getSecretValue({ SecretId: configExports.cognito_backend_access_key })
+    .promise();
+  const backendAdminCredentials = JSON.parse(
+    backendAdminCredentialsSecret.SecretString
+  );
 
-    const adminAuthResult = await idp.adminInitiateAuth({
-        AuthFlow: "ADMIN_NO_SRP_AUTH",
-        ClientId: configExports.cognito_backend_client_id,
-        UserPoolId: configExports.cognito_pool_id,
-        AuthParameters: {
-            "USERNAME": backendAdminCredentials.username,
-            "PASSWORD": backendAdminCredentials.password
-        }
-    }).promise();
+  const adminAuthResult = await idp
+    .adminInitiateAuth({
+      AuthFlow: "ADMIN_NO_SRP_AUTH",
+      ClientId: configExports.cognito_backend_client_id,
+      UserPoolId: configExports.cognito_pool_id,
+      AuthParameters: {
+        USERNAME: backendAdminCredentials.username,
+        PASSWORD: backendAdminCredentials.password,
+      },
+    })
+    .promise();
 
-    return adminAuthResult.AuthenticationResult;
-}
+  return adminAuthResult.AuthenticationResult;
+};
 
 const buildAppSyncClient = (backendTokens) => {
-    return new AWSAppSyncClient({
-        url: configExports.appsync_endpoint_url,
-        region: configExports.appsync_region,
-        auth: {
-            type: "AMAZON_COGNITO_USER_POOLS",
-            jwtToken: backendTokens.AccessToken
-        },
-        disableOffline: true
+  return new AWSAppSyncClient({
+    url: configExports.appsync_endpoint_url,
+    region: configExports.appsync_region,
+    auth: {
+      type: "AMAZON_COGNITO_USER_POOLS",
+      jwtToken: backendTokens.AccessToken,
+    },
+    disableOffline: true,
+  });
+};
+
+const getFeedApiKey = async (sm) => {
+  const dataFeedSecretResult = await sm
+    .getSecretValue({ SecretId: configExports.data_feed_key })
+    .promise();
+  return JSON.parse(dataFeedSecretResult.SecretString).feed_api_key;
+};
+
+const getPrices = async (apiKey, symbol) => {
+  const feedUrl =
+    "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" +
+    symbol +
+    "&interval=" +
+    configExports.feed_interval +
+    "&apikey=" +
+    apiKey;
+  console.log("FEEEEDD URRRRL", feedUrl);
+  const response = await axios(feedUrl);
+  const data = response.data;
+  if ("Note" in data) {
+    console.log("[" + symbol + "] Rate limit exceeded, skipping request");
+    return [];
+  } else if ("Error Message" in data) {
+    return [];
+  } else {
+    const metadata = data["Meta Data"];
+    const timezone = metadata["6. Time Zone"];
+    const feedPrices =
+      data["Time Series (" + configExports.feed_interval + ")"];
+
+    var prices = [];
+
+    for (i in feedPrices) {
+      const ts = moment.tz(i, timezone).unix();
+      const price = feedPrices[i];
+      prices.push({
+        id: symbol + "-" + ts,
+        symbol: symbol,
+        data_timezone: timezone,
+        data_timestamp: ts,
+        open_price: price["1. open"],
+        high_price: price["2. high"],
+        low_price: price["3. low"],
+        close_price: price["4. close"],
+        volume: price["5. volume"],
+      });
+    }
+
+    return prices.reverse();
+  }
+};
+
+const writePrices = async (client, prices) => {
+  for (i in prices) {
+    const price = prices[i];
+    try {
+      await client.mutate({
+        mutation: createIntradayStockPrice,
+        variables: { input: price },
+      });
+    } catch (e) {}
+  }
+};
+
+exports.handler = async (event) => {
+  const sm = new AWS.SecretsManager();
+  const backendTokens = await getBackendTokens(sm);
+  const client = buildAppSyncClient(backendTokens);
+  var nextToken = null;
+  const symbols = new Set();
+
+  const apiKey = await getFeedApiKey(sm);
+
+  do {
+    var results = await client.query({
+      query: listAllStockSymbols,
+      variables: { nextToken: nextToken },
     });
-}
 
-const getFeedApiKey = async(sm) => {
-    const dataFeedSecretResult = await sm.getSecretValue({SecretId: configExports.data_feed_key}).promise();
-    return JSON.parse(dataFeedSecretResult.SecretString).feed_api_key;
-}
+    const items = results.data.listAllStockSymbols.items;
+    nextToken = results.data.listAllStockSymbols.nextToken;
 
-const getPrices = async(apiKey, symbol) => {
-    const feedUrl = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+symbol+"&interval="+configExports.feed_interval+"&apikey="+apiKey;
-    const response = await axios(feedUrl);
-    const data = response.data;
-    if ("Note" in data) {
-        console.log("["+symbol+"] Rate limit exceeded, skipping request");
-        return [];
-    } else if ("Error Message" in data) {
-        return [];
-    } else {
-        const metadata = data["Meta Data"];
-        const timezone = metadata["6. Time Zone"];
-        const feedPrices = data["Time Series ("+configExports.feed_interval+")"];
-    
-        var prices = [];
-    
-        for (i in feedPrices) {
-            const ts = moment.tz(i, timezone).unix();
-            const price = feedPrices[i];
-            prices.push({
-                id: symbol+"-"+ts,
-                symbol: symbol,
-                data_timezone: timezone,
-                data_timestamp: ts,
-                open_price: price["1. open"],
-                high_price: price["2. high"],
-                low_price: price["3. low"],
-                close_price: price["4. close"],
-                volume: price["5. volume"]
-            });
+    if (items != null && items.length > 0) {
+      for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+
+        var symbol = item.symbol.toUpperCase();
+
+        if (!symbols.has(symbol)) {
+          const prices = await getPrices(apiKey, symbol);
+          if (prices.length > 0) {
+            await writePrices(client, prices);
+            symbols.add(symbol);
+          }
         }
-    
-        return prices.reverse();
+      }
     }
-}
-
-const writePrices = async(client, prices) => {
-    for (i in prices) {
-        const price = prices[i];
-        try {
-            await client.mutate({mutation: createIntradayStockPrice, variables: {input: price}});
-        } catch (e) {
-
-        }
-    }
-}
-
-exports.handler = async(event) => {
-    const sm = new AWS.SecretsManager();
-    const backendTokens = await getBackendTokens(sm);
-    const client = buildAppSyncClient(backendTokens);
-    var nextToken = null;
-    const symbols = new Set();
-
-    const apiKey = await getFeedApiKey(sm);
-
-    do {
-        var results = await client.query({query: listAllStockSymbols, variables: {nextToken: nextToken}});
-    
-        const items = results.data.listAllStockSymbols.items;
-        nextToken = results.data.listAllStockSymbols.nextToken;
-
-        if (items != null && items.length > 0) {
-            for (var i=0;i<items.length;i++) {
-                var item = items[i];
-
-                var symbol = item.symbol.toUpperCase();
-
-                if (!symbols.has(symbol)) {
-                    const prices = await getPrices(apiKey, symbol);
-                    if (prices.length > 0) {
-                        await writePrices(client, prices);
-                        symbols.add(symbol);
-                    }
-                }
-            }
-        }
-    } while (nextToken != null);
-}
+  } while (nextToken != null);
+};
