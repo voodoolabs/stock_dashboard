020b779de125bd0df91ba5769e946d5c87cc9d79
Nicolaas Smit <nick.smit@rtk.io>
2020-07-28 19:21:20 +0200

    initializing

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,69 +1,105 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const createIntradayStockPrice = `mutation CreateIntradayStockPrice($input: CreateIntradayStockPriceInput!) {
-  createIntradayStockPrice(input: $input) {
-    id
-    symbol
-    data_timezone
-    data_timestamp
-    open_price
-    high_price
-    low_price
-    close_price
-    volume
+export const createIntradayStockPrice = /* GraphQL */ `
+  mutation CreateIntradayStockPrice(
+    $input: CreateIntradayStockPriceInput!
+    $condition: ModelIntradayStockPriceConditionInput
+  ) {
+    createIntradayStockPrice(input: $input, condition: $condition) {
+      id
+      symbol
+      data_timezone
+      data_timestamp
+      open_price
+      high_price
+      low_price
+      close_price
+      volume
+      createdAt
+      updatedAt
+    }
   }
-}
 `;
-export const updateIntradayStockPrice = `mutation UpdateIntradayStockPrice($input: UpdateIntradayStockPriceInput!) {
-  updateIntradayStockPrice(input: $input) {
-    id
-    symbol
-    data_timezone
-    data_timestamp
-    open_price
-    high_price
-    low_price
-    close_price
-    volume
+export const updateIntradayStockPrice = /* GraphQL */ `
+  mutation UpdateIntradayStockPrice(
+    $input: UpdateIntradayStockPriceInput!
+    $condition: ModelIntradayStockPriceConditionInput
+  ) {
+    updateIntradayStockPrice(input: $input, condition: $condition) {
+      id
+      symbol
+      data_timezone
+      data_timestamp
+      open_price
+      high_price
+      low_price
+      close_price
+      volume
+      createdAt
+      updatedAt
+    }
   }
-}
 `;
-export const deleteIntradayStockPrice = `mutation DeleteIntradayStockPrice($input: DeleteIntradayStockPriceInput!) {
-  deleteIntradayStockPrice(input: $input) {
-    id
-    symbol
-    data_timezone
-    data_timestamp
-    open_price
-    high_price
-    low_price
-    close_price
-    volume
+export const deleteIntradayStockPrice = /* GraphQL */ `
+  mutation DeleteIntradayStockPrice(
+    $input: DeleteIntradayStockPriceInput!
+    $condition: ModelIntradayStockPriceConditionInput
+  ) {
+    deleteIntradayStockPrice(input: $input, condition: $condition) {
+      id
+      symbol
+      data_timezone
+      data_timestamp
+      open_price
+      high_price
+      low_price
+      close_price
+      volume
+      createdAt
+      updatedAt
+    }
   }
-}
 `;
-export const createStockSymbol = `mutation CreateStockSymbol($input: CreateStockSymbolInput!) {
-  createStockSymbol(input: $input) {
-    id
-    symbol
-    owner
+export const createStockSymbol = /* GraphQL */ `
+  mutation CreateStockSymbol(
+    $input: CreateStockSymbolInput!
+    $condition: ModelStockSymbolConditionInput
+  ) {
+    createStockSymbol(input: $input, condition: $condition) {
+      id
+      symbol
+      owner
+      createdAt
+      updatedAt
+    }
   }
-}
 `;
-export const updateStockSymbol = `mutation UpdateStockSymbol($input: UpdateStockSymbolInput!) {
-  updateStockSymbol(input: $input) {
-    id
-    symbol
-    owner
+export const updateStockSymbol = /* GraphQL */ `
+  mutation UpdateStockSymbol(
+    $input: UpdateStockSymbolInput!
+    $condition: ModelStockSymbolConditionInput
+  ) {
+    updateStockSymbol(input: $input, condition: $condition) {
+      id
+      symbol
+      owner
+      createdAt
+      updatedAt
+    }
   }
-}
 `;
-export const deleteStockSymbol = `mutation DeleteStockSymbol($input: DeleteStockSymbolInput!) {
-  deleteStockSymbol(input: $input) {
-    id
-    symbol
-    owner
+export const deleteStockSymbol = /* GraphQL */ `
+  mutation DeleteStockSymbol(
+    $input: DeleteStockSymbolInput!
+    $condition: ModelStockSymbolConditionInput
+  ) {
+    deleteStockSymbol(input: $input, condition: $condition) {
+      id
+      symbol
+      owner
+      createdAt
+      updatedAt
+    }
   }
-}
 `;
--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,17 +1,20 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const intradayStockPriceCreated = `subscription IntradayStockPriceCreated($symbol: String) {
-  intradayStockPriceCreated(symbol: $symbol) {
-    id
-    symbol
-    data_timezone
-    data_timestamp
-    open_price
-    high_price
-    low_price
-    close_price
-    volume
+export const intradayStockPriceCreated = /* GraphQL */ `
+  subscription IntradayStockPriceCreated($symbol: String) {
+    intradayStockPriceCreated(symbol: $symbol) {
+      id
+      symbol
+      data_timezone
+      data_timestamp
+      open_price
+      high_price
+      low_price
+      close_price
+      volume
+      createdAt
+      updatedAt
+    }
   }
-}
 `;
